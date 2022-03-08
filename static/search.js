a97a7b279b0225b48d77cdaeae53ec341ca2fb0f
christopher webb <chris.webb5249@gmail.com>
2022-03-08 02:41:03 -0500

    update

--- static/search.js
+++ static/search.js
@@ -1,9 +1,13 @@
 function search(term) {
-    window.location.assign("/search/" + term);
+    if (!term || term.length == 0) {
+        return 
+    } else {
+        window.location.assign("/search/" + term);
+    }
+   
  }
 
  $(document).ready(function() {
-
    $("#search_button").click(function() {
      let text = $("#search_input").val();
      search(text);
