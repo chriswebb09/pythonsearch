7a9b57521dab584fa1de127346d27e170bc4d400
christopher webb <chris.webb5249@gmail.com>
2022-03-07 23:02:24 -0500

    initial commit

---
+++ server.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify
+app = Flask(__name__)
+
+data = [
+    {
+        "title": "In Moscow's Shadows 60: Ukraine: Nuclear Options, National Morale, and How Kyiv Can Save Moscow",
+        "description": "What can one say about the unfolding horror in Ukraine. In this podcast I alight on a few specific issues: Putin's nuclear signalling (at least I hope that's all it is), the idiocy of 'No Fly Zones' in this context, Russian morale, and how, if Putin is re-booting the Brezhnev franchise, this could in the long-term let Russia finally complete its reform process.",
+        "podcast_name": "In Moscow's Shadows",
+        "date": "02/27/2022",
+        "url": "https://www.buzzsprout.com/1026985/10152168-in-moscow-s-shadows-60-ukraine-nuclear-options-national-morale-and-how-kyiv-can-save-moscow",
+        "recent_episodes": ["https://www.buzzsprout.com/1026985/10107958-in-moscow-s-shadows-59-imagining-a-ukrainian-peace-deal", "https://www.buzzsprout.com/1026985/10070465-in-moscow-s-shadows-58-ukrainian-thoughts-welcome-to-stagnation-and-more-2022-predictions"],
+        "podcast_id": 1
+    },
+     {
+        "title": "In Moscow's Shadows 60: Ukraine: Nuclear Options, National Morale, and How Kyiv Can Save Moscow",
+        "description": "What can one say about the unfolding horror in Ukraine. In this podcast I alight on a few specific issues: Putin's nuclear signalling (at least I hope that's all it is), the idiocy of 'No Fly Zones' in this context, Russian morale, and how, if Putin is re-booting the Brezhnev franchise, this could in the long-term let Russia finally complete its reform process.",
+        "podcast_name": "In Moscow's Shadows",
+        "date": "02/27/2022",
+        "url": "https://www.buzzsprout.com/1026985/10152168-in-moscow-s-shadows-60-ukraine-nuclear-options-national-morale-and-how-kyiv-can-save-moscow",
+        "recent_episodes": ["https://www.buzzsprout.com/1026985/10107958-in-moscow-s-shadows-59-imagining-a-ukrainian-peace-deal", "https://www.buzzsprout.com/1026985/10070465-in-moscow-s-shadows-58-ukrainian-thoughts-welcome-to-stagnation-and-more-2022-predictions"],
+        "podcast_id": 2
+    },
+    {
+        "title": "In Moscow's Shadows 60: Ukraine: Nuclear Options, National Morale, and How Kyiv Can Save Moscow",
+        "description": "What can one say about the unfolding horror in Ukraine. In this podcast I alight on a few specific issues: Putin's nuclear signalling (at least I hope that's all it is), the idiocy of 'No Fly Zones' in this context, Russian morale, and how, if Putin is re-booting the Brezhnev franchise, this could in the long-term let Russia finally complete its reform process.",
+        "podcast_name": "In Moscow's Shadows",
+        "date": "02/27/2022",
+        "url": "https://www.buzzsprout.com/1026985/10152168-in-moscow-s-shadows-60-ukraine-nuclear-options-national-morale-and-how-kyiv-can-save-moscow",
+        "recent_episodes": ["https://www.buzzsprout.com/1026985/10107958-in-moscow-s-shadows-59-imagining-a-ukrainian-peace-deal", "https://www.buzzsprout.com/1026985/10070465-in-moscow-s-shadows-58-ukrainian-thoughts-welcome-to-stagnation-and-more-2022-predictions"],
+        "podcast_id": 3
+    },
+]
+
+# ROUTES
+
+@app.route('/')
+def hello_world():
+   return render_template('welcome.html')
+
+@app.route('/details/<id>')
+def details(id=None):
+    item = data[id]
+    return render_template('detail.html')
+
+@app.route('/search/<term>')
+def search(term: None):
+    return render_template('search.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
